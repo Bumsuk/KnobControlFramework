f0d910880576101f3c03db73a7776713d99ce190
bumsuk <xenix72@gmail.com>
2020-02-11 13:41:41 +0900

    1차 pod 소스 반영 : 어디까지나 로컬 리포지토리에서 사용키위해 추가했으므로, 공개할 예정은 없음.

---
+++ KnobControlFramework/Common.swift
@@ -0,0 +1,13 @@
+//
+//  Common.swift
+//  KnobControlFramework
+//
+//  Created by brown on 2020/02/10.
+//  Copyright © 2020 brown. All rights reserved.
+//
+
+import Foundation
+
+public class Common {
+    public static func hello() { print("🤑 헬로우!") }
+}
---
+++ KnobControlFramework/KnobControlFramework.h
@@ -0,0 +1,19 @@
+//
+//  KnobControlFramework.h
+//  KnobControlFramework
+//
+//  Created by brown on 2020/02/10.
+//  Copyright © 2020 brown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KnobControlFramework.
+FOUNDATION_EXPORT double KnobControlFrameworkVersionNumber;
+
+//! Project version string for KnobControlFramework.
+FOUNDATION_EXPORT const unsigned char KnobControlFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControlFramework/PublicHeader.h>
+
+
